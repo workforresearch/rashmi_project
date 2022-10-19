44d7929b15f2d68ec0832d2c3d923bcd87058cec
workforresearch <115945903+workforresearch@users.noreply.github.com>
2022-10-19 07:29:29 +0530

    Update app.py

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -8,8 +8,10 @@ post_count = 0
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global connection_number
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    connection_number +=1
     return connection
 
 # Function to get a post using its ID
@@ -39,14 +41,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+        app.logger.error('Page not found')
         return render_template('404.html'), 404
     else:
-        post_count +=1
+        app.logger.info('retrieved '+ post[post_id])
         return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info ('About Us page is retrieved')
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -64,7 +68,7 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info('New post created' + title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
@@ -77,23 +81,32 @@ def status():
         status=200,
         mimetype='application/json'
   ) 
+    app.logger.info('Status request successfull')
     return response    
 
 
 # define the matrics endpoints
 @app.route('/metrics')
 def metrics():
+    global connection_number
+    post_count = get_post_count()
     response = app.response_class(
         response=json.dumps({"status":"success","code":0,"data":{"db_connection_count": connection_number, "post_count": post_count}}),
         status=200,
         mimetype='application/json'
   )
-    
+    app.logger.info('Metrics request successfull')
     return response
 
+def get_post_count():
+    connection = get_db_connection()
+    
+    posts = connection.execute('SELECT * FROM posts').fetchall()
+                     
+    connection.close()
+    return len(posts)
 
 # start the application on port 3111
 if __name__ == "__main__":
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0', port='3111')
-
